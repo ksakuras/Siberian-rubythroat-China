975e9228e4a56a8eca1dd04949a53dcd0d73abcf
ksakuras <49394209+ksakuras@users.noreply.github.com>
2023-03-21 16:46:54 +0100

    Add files via upload

--- analysis/0-preparation.R
+++ analysis/0-preparation.R
@@ -1,42 +1,57 @@
+#library(devtools)
+#devtools::install_github("Rafnuss/GeoPressureR")
+#devtools::install()
 
-# Set debug T to see all check and set to F once everything is correct
-debug <- F
-
-# Define the geolocator data logger id to use
-# gdl <- "5D6"
-
-raw <- read.csv(paste0("data/0_PAM/", gdl, "/", gdl, "_act_pres_temp.csv"))
-
-# change timestamp back to correct format
-raw$timestamp = as.POSIXct(raw$timestamp,tz = "GMT", format = c("%Y-%m-%d %H:%M:%S"))
-if (debug){
-  head(raw)
-  #CHECK whether it is GMT!
-  unique(raw$timestamp)
-  summary(raw)
-  str(raw)
-}
-
-# extract info
-pressure.mid = subset(raw, series == "pressure")
-acto.mid = subset(raw, series == "actoscore")
-temp.mid = subset(raw, series == "temperature")
-
-# combine all information into pam variable
-pam <- list(length = 4)  #create a list variable
-pam$pressure <- data.frame(pressure.mid[,c(2,3)])  #copy the column 2-3 to the list
-pam$acceleration <- data.frame(acto.mid[,c(2,3)])
-pam$temperature <- data.frame(temp.mid[,c(2,3)])
-pam$light <- data.frame(acto.mid[,c(2,3)])   #create a list of light with acto schedule
-pam$light[,2] <- rep(0,length(pam$light[,2])) #force all data to 0 as we don't have light data
-names(pam) <- c("id","pressure","acceleration","temperature","light") #change "length" list to name "id"
-pam$id <- as.character("5D6")
-colnames(pam$pressure) <- c("date","obs")  #change column name for each list
-colnames(pam$acceleration) <- c("date","obs")
-colnames(pam$light) <- c("date","obs")
-colnames(pam$temperature) <- c("date","obs")
-
-if (debug){
-  summary(pam)
-  str(pam)
-}
+library(GeoPressureR)
+# Only used for some visualization. The code to compute the light position is included in `GeoPressureR`
+library(GeoLocTools)
+setupGeolocation()
+# ERA5 data download library
+library(ecmwfr)
+# Graph library
+library(igraph)
+# Plotting library
+library(ggplot2)
+library(gridExtra)
+library(plotly)
+library(RColorBrewer)
+# Interactif figure library
+library(leaflet)
+library(leaflet.extras)
+#for 1-pressure
+library(GeoPressureR)
+library(leaflet)
+library(leaflet.providers)
+library(leaflet.extras)
+library(ggplot2)
+library(plotly)
+library(RColorBrewer)
+library(dplyr)
+library(raster)
+library(readxl)
+#for 3-static
+library(GeoPressureR)
+library(leaflet)
+library(leaflet.providers)
+library(leaflet.extras)
+library(ggplot2)
+library(plotly)
+library(RColorBrewer)
+library(raster)
+library(dplyr)
+library(readxl)
+#for 4-basic
+library(GeoPressureR)
+library(leaflet)
+library(leaflet.extras)
+library(raster)
+library(igraph)
+library(mapview)
+library(htmlwidgets)
+library(webshot)
+#for 5-wind
+library(GeoPressureR)
+library(leaflet)
+library(leaflet.extras)
+library(raster)
+library(igraph)
